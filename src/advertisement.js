43681b00a1d401805fc4737ec5b576e567a5f8a1
gin0606 <kkgn06@gmail.com>
2018-05-16 00:08:04 +0900

    remove ad

--- src/advertisement.js
+++ src/advertisement.js
@@ -2,18 +2,6 @@ var React = require("react");
 var ReactDOM = require("react-dom");
 
 var AdsenseAdvertisement = React.createClass({
-  showUpAdsense: function() {
-    var adsense = document.getElementById(
-      "adsense-original-div-" + this.props.type
-    );
-    adsense.className = "";
-    ReactDOM.findDOMNode(
-      this.refs["adsense-space-" + this.props.type]
-    ).appendChild(adsense);
-  },
-  componentDidMount: function() {
-    this.showUpAdsense();
-  },
   render: function() {
     return (
       <div
