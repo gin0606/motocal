3614b24dde9f7ed86a2f193447df29f80744302c
gin0606 <kkgn06@gmail.com>
2018-05-17 01:59:15 +0900

    Revert "remove ad"
    This reverts commit 43681b00a1d401805fc4737ec5b576e567a5f8a1.

--- src/AdsenseAdvertisement.js
+++ src/AdsenseAdvertisement.js
@@ -1,6 +1,20 @@
 var React = require("react");
 
 class AdsenseAdvertisement extends React.Component {
+  showUpAdsense() {
+    var adsense = document.getElementById(
+      "adsense-original-div-" + this.props.type
+    );
+    adsense.className = "";
+    ReactDOM.findDOMNode(
+      this.refs["adsense-space-" + this.props.type]
+    ).appendChild(adsense);
+  }
+
+  componentDidMount() {
+    this.showUpAdsense();
+  }
+
   render() {
     return (
       <div
